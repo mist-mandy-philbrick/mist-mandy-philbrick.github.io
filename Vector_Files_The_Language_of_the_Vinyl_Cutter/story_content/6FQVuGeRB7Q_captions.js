dd05ebee2f08ac8d1cb7b2fe2f85766bb9053e91
mist-mandy-philbrick <123275263+mist-mandy-philbrick@users.noreply.github.com>
2023-09-12 13:45:15 -0700

    Vector Files Module Upload

---
+++ Vector_Files_The_Language_of_the_Vinyl_Cutter/story_content/6FQVuGeRB7Q_captions.js
@@ -0,0 +1,1 @@
+window.globalProvideData('caption', '{"data":"WEBVTT%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%206.79.30921.0%0D%0A%0D%0A00:00:00.065%20--%3E%2000:00:05.547%0D%0ANow%20let\u0027s%20go%20over%20some%20key%20points%20about%20vector%20files%20and%20raster%20files.%0D%0A%0D%0A"}')
