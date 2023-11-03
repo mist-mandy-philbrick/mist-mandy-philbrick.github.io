2458fb9e064ba465dd563deaf7574d978f6ace2c
mist-mandy-philbrick <123275263+mist-mandy-philbrick@users.noreply.github.com>
2023-11-02 19:18:49 -0700

    intro module updates to audio

--- Introduction_Vinyl_Cutter_Course/story_content/6BfdTHnXPHI_captions.js
+++
@@ -1,1 +0,0 @@
-window.globalProvideData('caption', '{"data":"WEBVTT%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%206.80.31058.0%0D%0A%0D%0A00:00:00.032%20--%3E%2000:00:01.789%0D%0AWhen%20you\u0027re%20ready%20to%20continue%0D%0A%0D%0A00:00:01.846%20--%3E%2000:00:04.324%0D%0Aselect%20the%20%22Next%22%20button.%0D%0A%0D%0A"}')
