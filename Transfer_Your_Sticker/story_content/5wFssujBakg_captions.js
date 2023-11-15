a42c2c5ea018b98811d4684b6202df78db575069
mist-mandy-philbrick <123275263+mist-mandy-philbrick@users.noreply.github.com>
2023-11-15 15:03:40 -0800

    updates to multiple modules, audio added

---
+++ Transfer_Your_Sticker/story_content/5wFssujBakg_captions.js
@@ -0,0 +1,1 @@
+window.globalProvideData('caption', '{"data":"WEBVTT%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%206.81.31200.0%0D%0A%0D%0A00:00:00.128%20--%3E%2000:00:01.744%0D%0AAfer%20watching%20this%20video,%0D%0A%0D%0A00:00:01.862%20--%3E%2000:00:04.800%0D%0Aselect%20%22Next%22%20to%20continue%20to%20review%20slides.%0D%0A%0D%0A"}')
